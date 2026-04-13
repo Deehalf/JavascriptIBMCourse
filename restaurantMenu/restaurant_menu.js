d9420536e350804a11533445d1e7ed520f6de306
deehalf <ac.sacross@gmail.com>
2026-04-13 02:02:12 -0400

    agregados precios a los productos del menu

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $25', 'Oatmeal - $13', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $30', 'Pasta - $20', 'Burger - $15', 'Salmon - $25'];
+const dessertMenu = ['Cake - $8', 'Ice Cream - $5', 'Pudding - $6', 'Fruit Salad - $7'];
 
  const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
         document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
